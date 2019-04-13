bdc13bed1211a06edc18f3a99f7524f315351493
andreibudoi <35580279+andreibudoi@users.noreply.github.com>
2019-04-14 01:11:40 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -9,22 +9,22 @@ using namespace std;
 
 int main()
 {
-	
+
 	fstream f("date2.in");
 
-	char alfabet[100],noduri[100];
+	char alfabet[100], noduri[100];
 
 	int *ascii = new int[300];
 	int *asciinoduri = new int[300];
 	memset(ascii, -1, 300 * sizeof(int));
 	memset(asciinoduri, -1, 300 * sizeof(int));
 
-	int inverse=0,prod;
-	
+	int inverse = 0, prod;
+
 
 	f >> noduri >> alfabet;
-	
-	
+
+
 	int nr_nod = strlen(noduri);
 	int index_noduri = 0;
 	for (int i = 0; i < nr_nod; i++)
@@ -44,14 +44,14 @@ int main()
 		index_alfabet++;
 
 	}
-	vector<int> **functie= new vector<int>*[nr_nod+1];
-	for (int i = 0; i < nr_nod+1; i++)
+	vector<int> **functie = new vector<int>*[nr_nod + 1];
+	for (int i = 0; i < nr_nod + 1; i++)
 		functie[i] = new vector<int>[nr_alf];
 
 
 	f >> prod;
-	char x,y[5];
-	
+	char x, y[5];
+
 	for (int i = 0; i < prod; i++)
 	{
 		f >> x;
@@ -71,7 +71,7 @@ int main()
 		if (ascii[y[0]] == -1)
 			inverse = 1;
 		if (inverse)
-				functie[asciinoduri[x]][ascii[y[1]]].push_back(asciinoduri[y[0]]);
+			functie[asciinoduri[x]][ascii[y[1]]].push_back(asciinoduri[y[0]]);
 		else	functie[asciinoduri[x]][ascii[y[0]]].push_back(asciinoduri[y[1]]);
 
 
@@ -79,7 +79,7 @@ int main()
 
 	int curr[100], inter[100];
 	char word[100];
-	int index_curr/*ind*/, index_inter/*ind1*/,lit;
+	int index_curr/*ind*/, index_inter/*ind1*/, lit;
 	while (f >> word)
 	{
 		if (inverse)_strrev(word);
@@ -99,10 +99,10 @@ int main()
 				curr[j] = inter[j];
 
 			index_curr = index_inter;
-		
+
 		}
 		int ok = 0;
-		for(int i=0;i<index_curr;i++)
+		for (int i = 0; i < index_curr; i++)
 			if (fin[curr[i]] == 1)
 			{
 				ok = 1;
@@ -110,10 +110,11 @@ int main()
 			}
 		if (inverse)_strrev(word);
 		cout << word << " este ";
+
 		if (ok == 0)cout << "respins\n";
 		else cout << "ok\n";
 	}
-	
+
 	for (int i = 0; i < nr_nod; i++)
 		delete[] functie[i];
 	delete[] functie;
